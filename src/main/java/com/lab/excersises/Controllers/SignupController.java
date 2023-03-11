4cbc2f39b0117da6153ad21ee5b514934cc1cc6e
mohhammednasr <mohammed2011345@miuegypt.edu.eg>
2023-03-11 10:18:33 +0200

    done

--- src/main/java/com/lab/excersises/Controllers/SignupController.java
+++ src/main/java/com/lab/excersises/Controllers/SignupController.java
@@ -1,10 +1,11 @@
 package com.lab.excersises.Controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class SignupController {
     @GetMapping("/signup")
     public String showSignupForm() {
@@ -13,6 +14,6 @@ public class SignupController {
 
     @PostMapping("/signup")
     public String submitSignupForm() {
-        return "redirect:/home";
+        return "redirect:/home.html";
     }
 }
